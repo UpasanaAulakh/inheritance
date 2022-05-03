6099173139ae9f95325304e9d389df40363bce6a
admin <admin@CHETUIWK385>
2022-05-03 19:15:38 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+public class Animal
+{
+    public void eat()
+    
+    { Console.WriteLine(" animal can Eat..."); }
+}
+public class Dog : Animal
+{
+    public void bark() 
+    
+    { Console.WriteLine("Dog can bark..."); }
+}
+public class BabyDog : Dog
+{
+    public void weep()
+    
+    { Console.WriteLine("Weeping..."); }
+}
+class Animal2
+{
+    public static void Main(string[] args)
+    {
+        BabyDog d1 = new BabyDog();
+        d1.eat();
+        d1.bark();
+        d1.weep();
+    }
+}
